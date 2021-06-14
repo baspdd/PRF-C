0f2b24d290309b63a2b5928130bb90113d257b7c
elajini <84675442+elajini@users.noreply.github.com>
2021-06-14 18:54:06 +0700

    Update vong` for long` nhau.cpp

--- vong` for long` nhau.cpp
+++ vong` for long` nhau.cpp
@@ -4,10 +4,10 @@
 int main() {
    int a,b,c,d,w,i,j,m,n,s=0;
    scanf("%ld%ld%ld%ld%ld", &a,&b,&c,&d,&w);
-   for (i=0;i<=w;i++)
-     for (j=0;j<=w;j++)
-      for (m=0;m<=w;m++)
-       for (n=0;n<=w;n++)
+   for (i=0;i<=w/i;i++)
+     for (j=0;j<=w/j;j++)
+      for (m=0;m<=w/m;m++)
+       for (n=0;n<=w/n;n++)
        {if (a*i+b*j+c*m+d*n==w) s++;
        
 	   }
