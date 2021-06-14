336dbc15fcf1271a2802551ee86f9bd06c1dd0a6
elajini <84675442+elajini@users.noreply.github.com>
2021-06-14 18:50:15 +0700

    https://ucode.vn/problems/money-44654

---
+++ vong` for long` nhau.cpp
@@ -0,0 +1,33 @@
+/*#include<stdio.h>
+#include<string.h>
+
+int main() {
+   int a,b,c,d,w,i,j,m,n,s=0;
+   scanf("%ld%ld%ld%ld%ld", &a,&b,&c,&d,&w);
+   for (i=0;i<=w;i++)
+     for (j=0;j<=w;j++)
+      for (m=0;m<=w;m++)
+       for (n=0;n<=w;n++)
+       {if (a*i+b*j+c*m+d*n==w) s++;
+       
+	   }
+	   printf("%lld",s);
+    
+    return 0;}*/
+
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+   int a,b,c,d,w,i,j,m,n,s=0;
+   scanf("%ld%ld%ld%ld%ld", &a,&b,&c,&d,&w);
+   for (i=0;i<=w;i++)
+   {for (j=0;j<=w;j++)
+   {for (m=0;m<=w;m++)
+   {for (n=0;n<=w;n++)
+       {if (a*i+b*j+c*m+d*n==w) s++;
+       
+	   }}}}
+	   printf("%lld",s);
+    
+    return 0;}
