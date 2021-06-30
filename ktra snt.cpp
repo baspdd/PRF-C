7b35dc3538ecac0675c6da31ea1e24891baaf914
elajini <84675442+elajini@users.noreply.github.com>
2021-06-30 08:36:59 +0700

    Update ktra snt.cpp

--- ktra snt.cpp
+++ ktra snt.cpp
@@ -11,6 +11,20 @@ long ktra(long n)
         }
     return check;}
 
+int soNguyenTo(int soA)
+{
+    if (soA < 2)    
+        return 0;
+
+    for (int i = 2; i <= sqrt((float)soA); i ++)
+    {
+        if (soA%i==0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int main()
 {
